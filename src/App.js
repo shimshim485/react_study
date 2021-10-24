bc7c05cae3961182631cb0611c8124c9b85ef137
shimshim485 <50603345+shimshim485@users.noreply.github.com>
2021-10-24 19:27:34 +0900

    committing react_study

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React,{Component} from 'react';
 import ValidationSample from './ValidationSample';
 import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 
 class App extends Component{
   render(){
     return (
-      <SassComponent/>
+      <StyledComponent/>
     );
   }
 }
